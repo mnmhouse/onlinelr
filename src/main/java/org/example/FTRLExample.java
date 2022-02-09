ea705fd89c606d42a0f3e19fbe94922a46a8de89
house.zhang <>
2022-02-09 09:56:34 +0800

    add kafka source op

--- src/main/java/org/example/FTRLExample.java
+++ src/main/java/org/example/FTRLExample.java
@@ -3,12 +3,15 @@ package org.example;
 import com.alibaba.alink.operator.batch.BatchOperator;
 import com.alibaba.alink.operator.batch.classification.LogisticRegressionTrainBatchOp;
 import com.alibaba.alink.operator.batch.source.CsvSourceBatchOp;
+import com.alibaba.alink.operator.stream.StreamOperator;
 import com.alibaba.alink.operator.stream.dataproc.JsonValueStreamOp;
 import com.alibaba.alink.operator.stream.dataproc.SplitStreamOp;
 import com.alibaba.alink.operator.stream.evaluation.EvalBinaryClassStreamOp;
 import com.alibaba.alink.operator.stream.onlinelearning.FtrlPredictStreamOp;
 import com.alibaba.alink.operator.stream.onlinelearning.FtrlTrainStreamOp;
+import com.alibaba.alink.operator.stream.source.BaseSourceStreamOp;
 import com.alibaba.alink.operator.stream.source.CsvSourceStreamOp;
+import com.alibaba.alink.operator.stream.source.KafkaSourceStreamOp;
 import com.alibaba.alink.pipeline.Pipeline;
 import com.alibaba.alink.pipeline.PipelineModel;
 import com.alibaba.alink.pipeline.dataproc.StandardScaler;
@@ -38,12 +41,14 @@ public class FTRLExample {
                 "site_id", "site_domain", "device_id", "device_model"};
         String[] numericalColNames = new String[]{
                 "C14", "C15", "C16", "C17", "C18", "C19", "C20", "C21"};
-        // result column name of feature engineering
+
+
         String vecColName = "vec";
         int numHashFeatures = 30000;
 
 
-        // 1 setup feature engineering pipeline 构建特征工程流水线
+        // part1 离线模型训练
+        // 1  构建特征工程流水线
         Pipeline featurePipeline = new Pipeline()
                 .add(
                         new StandardScaler()
@@ -58,17 +63,9 @@ public class FTRLExample {
                 );
         //  fit feature pipeline model
         PipelineModel featurePipelineModel = featurePipeline.fit(trainBatchData);
-        //  prepare stream train data
-        CsvSourceStreamOp data = new CsvSourceStreamOp()
-                //.setFilePath("http://alink-release.oss-cn-beijing.aliyuncs.com/data-files/avazu-ctr-train-8M.csv")
-                .setFilePath("/Users/haozhang/Downloads/avazu-ctr-train-8M.csv")
-                .setSchemaStr(schemaStr)
-                .setIgnoreFirstLine(true);
-        // split stream to train and eval data 对于流数据源进行实时切分得到原始训练数据和原始预测数据
-        SplitStreamOp splitter = new SplitStreamOp().setFraction(0.5).linkFrom(data);
 
 
-        // train initial batch model 训练出一个逻辑回归模型作为FTRL算法的初始模型，这是为了系统冷启动的需要。
+        // 训练出一个逻辑回归模型作为FTRL算法的初始模型，这是为了系统冷启动的需要。
         LogisticRegressionTrainBatchOp lr = new LogisticRegressionTrainBatchOp()
                 .setVectorCol(vecColName)
                 .setLabelCol(labelColName)
@@ -76,6 +73,15 @@ public class FTRLExample {
                 .setMaxIter(10);
         BatchOperator<?> initModel = featurePipelineModel.transform(trainBatchData).link(lr);
 
+
+
+        // part 2 在线训练部分
+
+        // 准备数据流数据，这里的数据源 可以设置为诸如Kafka之类的数据
+        BaseSourceStreamOp  data = getSourceStreamOp(schemaStr);
+
+        // split stream to train and eval data 对于流数据源进行实时切分得到原始训练数据和原始预测数据
+        SplitStreamOp splitter = new SplitStreamOp().setFraction(0.5).linkFrom(data);
 
         // ftrl train 在初始模型基础上进行FTRL在线训练
         FtrlTrainStreamOp model = new FtrlTrainStreamOp(initModel)
@@ -117,4 +123,26 @@ public class FTRLExample {
                 )
                 .print();
     }
+
+    private static BaseSourceStreamOp<CsvSourceStreamOp> getSourceStreamOp(String schemaStr) {
+        // 准备数据流数据，这里的数据可以设置为诸如Kafka 流式数据
+        CsvSourceStreamOp data = new CsvSourceStreamOp()
+                //.setFilePath("http://alink-release.oss-cn-beijing.aliyuncs.com/data-files/avazu-ctr-train-8M.csv")
+                .setFilePath("/Users/haozhang/Downloads/avazu-ctr-train-8M.csv")
+                .setSchemaStr(schemaStr)
+                .setIgnoreFirstLine(true);
+        return data;
+    }
+
+    private static BaseSourceStreamOp<KafkaSourceStreamOp> getKafakSourceStreamOp(String schemaStr) {
+        // 准备数据流数据，这里的数据可以设置为诸如Kafka 流式数据
+        KafkaSourceStreamOp soure = new KafkaSourceStreamOp()
+                .setBootstrapServers("localhost:9092")
+                .setTopic("train_data_topic")
+                .setStartupMode("EARLIEST")
+                .setGroupId("");
+
+        return soure;
+    }
+
 }
